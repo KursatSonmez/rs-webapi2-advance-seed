ba7687621c9946e4f46b902be21adb6b419584a0
QNILL\\qnill <anil.yildirim@remmsoft.com>
2017-08-30 16:29:26 +0300

    Core-1 : GetByID and AutoCompleteList methods changed for async call.

--- RS.Core.Service/Base/Services/ICRUDService.cs
+++ RS.Core.Service/Base/Services/ICRUDService.cs
@@ -13,7 +13,7 @@ namespace RS.Core.Service
         Task<APIResult> Add(A model, Guid UserID, bool isCommit = true);
         Task<APIResult> Update(U model, Guid? UserID = null, bool isCommit = true, bool checkAuthorize = false);
         Task<APIResult> Delete(Y id, Guid? UserID = null, bool isCommit = true, bool checkAuthorize = false);
-        G GetByID(Y id, Guid? UserID = null, bool isDeleted = false);
-        IList<AutoCompleteListVM<Y>> AutoCompleteList(Y? id = null, string Text = null);
+        Task<G> GetByID(Y id, Guid? UserID = null, bool isDeleted = false);
+        Task<IList<AutoCompleteListVM<Y>>> AutoCompleteList(Y? id = null, string Text = null);
     }
 }
