a84504717ca664bd9a6693feced8a4a004a10c7e
qnill <anilyildirim12@gmail.com>
2017-09-08 22:35:25 +0300

    Fixes #27 : Sample enum class added.

---
+++ RS.Core.Const/Enum.cs
@@ -0,0 +1,21 @@
+﻿namespace RS.Core.Const
+{
+    /// <summary>
+    /// ToDo : Translate
+    /// Enum değerleri asal sayılardan oluşturulmuştur.
+    /// Multi seçim sırasında toplam değer, asal bölenlerine ayırılıp seçimler elde edilebilir.
+    /// </summary>
+    public class Enum
+    {
+        #region General
+        public enum fooDEnum : byte
+        {
+            fooOne = 2,
+            fooTwo = 3,
+            fooThree = 5,
+            fooFour = 7
+        }
+
+        #endregion
+    }
+}
